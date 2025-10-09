234811a6607988ee6d018edf531b5669e52f2f14
Rajdeep5270 <rajdeepex5270@gmail.com>
2025-10-09 17:39:12 +0530

    Queue Program

---
+++ Ch - 21 Queue/linkedListQueue.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+
+class Queue
+{
+private:
+    Node *head;
+    Node *front;
+    Node *rear;
+    int count;
+};
